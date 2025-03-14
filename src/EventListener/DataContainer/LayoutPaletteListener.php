5b85fa527124b8a6018b2c616e957c19275f398a
Hendrik Bengs <hben@designerei-werbeagentur.de>
2025-03-14 10:51:07 +0100

    Adjusted palette manipulation

--- src/EventListener/DataContainer/LayoutPaletteListener.php
+++ src/EventListener/DataContainer/LayoutPaletteListener.php
@@ -9,7 +9,7 @@ use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 use Contao\DataContainer;
 
 #[AsCallback(table: 'tl_content', target: 'config.onpalette')]
-class LayoutListener
+class LayoutPaletteListener
 {
     public function __invoke(string $palette, DataContainer $dc): string
     {
@@ -26,15 +26,15 @@ class LayoutListener
         }
 
         if ($parentRecord['layoutType'] == 'grid') {
-            return PaletteManipulator::create()
-                ->addField(['gridColumn', 'gridRow', 'order', 'alignmentSelf'], 'layout_legend', PaletteManipulator::POSITION_PREPEND)
+            $palette = PaletteManipulator::create()
+                ->addField(['gridColumn', 'gridRow', 'order', 'alignmentSelf'], 'layout_legend', PaletteManipulator::POSITION_APPEND)
                 ->applyToString($palette)
             ;
         } elseif ($parentRecord['layoutType'] == 'flex') {
-            return PaletteManipulator::create()
-                ->addField(['flexBasis', 'flex', 'flexGrow', 'flexShrink', 'order', 'alignmentSelf'], 'layout_legend', PaletteManipulator::POSITION_PREPEND)
+            $palette = PaletteManipulator::create()
+                ->addField(['flexBasis', 'flex', 'flexGrow', 'flexShrink', 'order', 'alignmentSelf'], 'layout_legend', PaletteManipulator::POSITION_APPEND)
                 ->applyToString($palette)
-                ;
+            ;
         }
 
         return $palette;
