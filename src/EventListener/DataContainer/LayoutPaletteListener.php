1b579d207a479bd395621b402e71ee6a3ebc18bb
Hendrik Bengs <hben@designerei-werbeagentur.de>
2025-09-23 13:32:50 +0200

    Reverted position of layout fields

--- src/EventListener/DataContainer/LayoutPaletteListener.php
+++ src/EventListener/DataContainer/LayoutPaletteListener.php
@@ -27,17 +27,17 @@ class LayoutPaletteListener
 
         if ($parentRecord['layoutType'] == 'grid') {
             $palette = PaletteManipulator::create()
-                ->addField(['gridColumn', 'gridRow', 'order', 'alignmentSelf'], 'layout_legend', PaletteManipulator::POSITION_PREPEND)
+                ->addField(['gridColumn', 'gridRow', 'order', 'alignmentSelf'], 'layout_legend', PaletteManipulator::POSITION_APPEND)
                 ->applyToString($palette)
             ;
         } elseif ($parentRecord['layoutType'] == 'flex') {
             $palette = PaletteManipulator::create()
-                ->addField(['flexBasis', 'flex', 'flexGrow', 'flexShrink', 'order', 'alignmentSelf'], 'layout_legend', PaletteManipulator::POSITION_PREPEND)
+                ->addField(['flexBasis', 'flex', 'flexGrow', 'flexShrink', 'order', 'alignmentSelf'], 'layout_legend', PaletteManipulator::POSITION_APPEND)
                 ->applyToString($palette)
             ;
         } elseif ($parentRecord['layoutType'] == 'columns') {
             $palette = PaletteManipulator::create()
-                ->addField(['break'], 'layout_legend', PaletteManipulator::POSITION_PREPEND)
+                ->addField(['break'], 'layout_legend', PaletteManipulator::POSITION_APPEND)
                 ->applyToString($palette)
             ;
         }
